72d583b7a9d39d051e68eb415d063a76f9c96ed6
zenliarth <54330804+zenliarth@users.noreply.github.com>
2020-09-04 16:16:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function clicar() {
+    var btn = document.querySelector(".btn-share")
+    var img = document.getElementById('imgShareBranco')
+    var share = document.querySelector(".share")
+    share.style.display = 'flex'
+    btn.style.background = 'hsl(217, 19%, 35%)'
+    img.src = "/article-preview-component-master/images/share-branco-pequeno.png"
+}
+function saiu() {
+    var btn = document.querySelector(".btn-share")
+    var img = document.getElementById('imgShareBranco')
+    var share = document.querySelector(".share")
+    share.style.display= 'none'
+    btn.style.background = 'hsla(211, 51%, 88%, 0.842)'
+    img.src = "/article-preview-component-master/images/share-azul.png"
+}
